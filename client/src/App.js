5ca034077c3e1cc95efe6234d156986c0b81cc1f
peterling7710 <PLing3@its.jnj.com>
2020-02-12 16:58:52 -0500

    Added client container and nginx routing

--- client/src/App.js
+++ client/src/App.js
@@ -10,27 +10,27 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link to="/">Home</Link>
-        <Link to="/otherpage">Other Page</Link>
-      </header>
-      <div>
-        <Route exact path="/" component={Fib}/>
-        <Route exact path="/otherpage" component={OtherPage}/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib}/>
+          <Route exact path="/otherpage" component={OtherPage}/>
 
-      </div>
+        </div>
       </div>
 
     </Router>
